948544ebf96c6f8d83a895a3b2c7e05788191047
ORazB <devandramahottama@gmail.com>
2025-12-07 21:17:11 +0800

    adding notes to math question

--- Math/1523. Count Odd Numbers in an Interval Range/code.py
+++ Math/1523. Count Odd Numbers in an Interval Range/code.py
@@ -1,3 +1,16 @@
+
+# Note:
+# Counter doesn't work because the strict constraint (it's too slow).
+
+# That's why we need to find a formula to calculate the number of odd numbers in the range [low, high].
+# The formula is as follows:
+# 1. If low is even, increment low by 1 to make it odd.
+# 2. If high is even, decrement high by 1 to make it odd.
+# 3. The number of odd numbers in the range [low, high] can be calculated using the formula: (high - low) // 2 + 1.
+
+# Why this works?
+# I HAVE NO IDEA LOL, I'LL COME BACK TO THIS LATER. 12/7/2025 (Still on Binary Search Topic, Math is the last topic.)
+
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
 
