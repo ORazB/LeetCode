56cb4f46b32f22e7c96f244e3a723f1f99d218f9
ORazB <devandramahottama@gmail.com>
2026-03-15 18:40:14 +0800

    138. Copy List with Random Pointer | Time Complexity: O(n) | Space Complexity: O(n)

---
+++ Linked List/138. Copy List with Random Pointer/hash.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+# Time Complexity: O(n)
+# Space Complexity: O(n) | Can be optimized to O(1) just by changing the nodes and disconnecting the old one
+
+
+class Solution:
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+
+        # Hash Method
+        temp = {}
+        curr = head
+        while curr:
+            temp[curr] = Node(curr.val)
+            curr = curr.next
+
+        curr = head
+        while curr:
+            temp[curr].next = temp.get(curr.next)
+            temp[curr].random = temp.get(curr.random)
+            curr = curr.next
+        return temp[head]
