7637bf61722495d47cc5e949d8438717a1c5901f
ORazB <devandramahottama@gmail.com>
2026-03-29 16:29:59 +0800

    2839. Check if Strings Can be Made Equal With Operations I | Time Complexity: O(n) | Space Complexity: O(n)

--- String Manipulation/2839. Check if Strings Can be Made Equal With Operations I/code.py
+++ String Manipulation/2839. Check if Strings Can be Made Equal With Operations I/code.py
@@ -1,15 +1,15 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
 
-        # O(n) solution (hash set)
+        # O(n) solution
         if len(s1) != len(s2):
             return False
-        
+
         s1Even = {}
         s1Odd = {}
         s2Even = {}
         s2Odd = {}
-        
+
         for i in range(len(s1)):
             if i % 2 == 0:
                 s1Even[s1[i]] = s1Even.get(s1[i], 0) + 1
@@ -17,5 +17,5 @@ class Solution:
             else:
                 s1Odd[s1[i]] = s1Odd.get(s1[i], 0) + 1
                 s2Odd[s2[i]] = s2Odd.get(s2[i], 0) + 1
-        
+
         return s1Even == s2Even and s1Odd == s2Odd
