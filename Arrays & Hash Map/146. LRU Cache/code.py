ba653b557657a25c2041874fcbecff3563f6230a
ORazB <devandramahottama@gmail.com>
2026-04-08 22:36:16 +0800

    REWRITING THIS UGLY AHH CODE | still can't fully understand the linked list & hashmap connection

--- Arrays & Hash Map/146. LRU Cache/code.py
+++ Arrays & Hash Map/146. LRU Cache/code.py
@@ -1,44 +1,14 @@
-class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
-
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}
-
-        self.head, self.tail = Node(0, 0), Node(0, 0)
-
-        self.head.next, self.tail.prev = self.tail, self.head
-    
-    def remove(self, node):
-        prev = node.prev
-        nxt = node.next
-
-        prev.next = nxt
-        nxt.prev = prev
-    
-    def insert(self, node):
-        prev, nxt = self.latest.prev, self.latest
+        
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        self.latest = Node(cache[key], cache[key].val)
-
+        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+        
 
-        if len(self.cache) > self.cap:
-            lru = self.oldest.next
-            self.remove(lru)
-            del self.cache[lru.key]
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
